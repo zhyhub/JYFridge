271746a4704b937b69785461a46960ac040b17eb
Torando1015 <760982661@qq.com>
2017-12-19 11:31:51 +0800

    监听wifi状态

--- app/src/main/java/smartlink/zhy/jyfridge/service/VoiceService.java
+++ app/src/main/java/smartlink/zhy/jyfridge/service/VoiceService.java
@@ -261,8 +261,6 @@ public class VoiceService extends AccessibilityService {
         public void onCompleted(SpeechError error) {
             if (error == null) {
                 showTip("播放完成");
-                // 移动数据分析，收集开始听写事件
-                FlowerCollector.onEvent(VoiceService.this, "iat_recognize");
                 mIatResults.clear();
                 // 设置参数
                 setParam();
@@ -328,7 +326,10 @@ public class VoiceService extends AccessibilityService {
                 if (mTts.isSpeaking()) {
                     mTts.stopSpeaking();
                 }
-                int code = mTts.startSpeaking("有什么吩咐", mTtsListener);
+                if (mIat.isListening()) {
+                    mIat.stopListening();
+                }
+                int code = mTts.startSpeaking("我在，您说", mTtsListener);
                 /*
                  * 只保存音频不进行播放接口,调用此接口请注释startSpeaking接口
 		         * text:要合成的文本，uri:需要保存的音频全路径，listener:回调接口
@@ -486,7 +487,7 @@ public class VoiceService extends AccessibilityService {
         // 设置语言区域
         mIat.setParameter(SpeechConstant.ACCENT, "mandarin");
         // 设置语音前端点:静音超时时间，即用户多长时间不说话则当做超时处理
-        mIat.setParameter(SpeechConstant.VAD_BOS, "4000");
+        mIat.setParameter(SpeechConstant.VAD_BOS, "10000");
         // 设置语音后端点:后端点静音检测时间，即用户停止说话多长时间内即认为不再输入， 自动停止录音
         mIat.setParameter(SpeechConstant.VAD_EOS, "1000");
         // 设置标点符号,设置为"0"返回结果无标点,设置为"1"返回结果有标点
@@ -527,6 +528,9 @@ public class VoiceService extends AccessibilityService {
             @Override
             public void onSuccess(Object o) {
                 L.e(TAG, "onSuccess" + o.toString());
+                if(mIat.isListening()){
+                    mIat.stopListening();
+                }
                 Gson gson = new Gson();
                 BaseEntity entity = gson.fromJson(o.toString(), BaseEntity.class);
                 if (entity.getCode() == 1) {

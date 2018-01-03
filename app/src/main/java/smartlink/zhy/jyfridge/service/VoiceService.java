7b283073c2f61b5c6572022de4d84922fccccdb6
Torando1015 <760982661@qq.com>
2018-01-03 13:38:00 +0800

    2018.1.3  13:37

--- app/src/main/java/smartlink/zhy/jyfridge/service/VoiceService.java
+++ app/src/main/java/smartlink/zhy/jyfridge/service/VoiceService.java
@@ -546,7 +546,7 @@ public class VoiceService extends AccessibilityService {
         // 设置语音前端点:静音超时时间，即用户多长时间不说话则当做超时处理
         mIat.setParameter(SpeechConstant.VAD_BOS, "10000");
         // 设置语音后端点:后端点静音检测时间，即用户停止说话多长时间内即认为不再输入， 自动停止录音
-        mIat.setParameter(SpeechConstant.VAD_EOS, "1000");
+        mIat.setParameter(SpeechConstant.VAD_EOS, "10000");
         // 设置标点符号,设置为"0"返回结果无标点,设置为"1"返回结果有标点
         mIat.setParameter(SpeechConstant.ASR_PTT, "1");
         // 设置音频保存路径，保存音频格式支持pcm、wav，设置路径为sd卡请注意WRITE_EXTERNAL_STORAGE权限
